7c48cf197c447c4edbaa9984b3764d2acde4be16
AmitaiB <blickstein@gmail.com>
2015-06-15 00:30:43 -0400

    finishes lab, 12 fails out of 25 remain.

--- OOPCardsSimpleViews/CardSwitcherViewController.m
+++ OOPCardsSimpleViews/CardSwitcherViewController.m
@@ -29,18 +29,40 @@
 - (IBAction)threeOfSpades:(id)sender {
     PlayingCard *threeOfSpades = [[PlayingCard alloc] initWithSuit:@"♠️" rank:@3];
     
+    NSString *stringIWantToDisplay = [NSString stringWithFormat:(@"%@ %@", (NSString*)threeOfSpades.rank, threeOfSpades.suit)];
+    NSLog(stringIWantToDisplay);
+    self.topLabel.text = stringIWantToDisplay;
+    self.middleLabel.text = stringIWantToDisplay;
+    self.bottomLabel.text = stringIWantToDisplay;
 }
 
 - (IBAction)fourOfClubs:(id)sender {
     PlayingCard *fourOfClubs = [[PlayingCard alloc] initWithSuit:@"♣️" rank:@4];
+    
+    NSString *stringIWantToDisplay = [NSString stringWithFormat:(@"%@ %@", fourOfClubs.rank, fourOfClubs.suit)];
+    self.topLabel.text = stringIWantToDisplay;
+    self.middleLabel.text = stringIWantToDisplay;
+    self.bottomLabel.text = stringIWantToDisplay;
 }
 
 - (IBAction)eightOfDiamonds:(id)sender {
     PlayingCard *eightOfDiamonds = [[PlayingCard alloc] initWithSuit:@"♦️" rank:@8];
+    
+    NSString *stringIWantToDisplay = [NSString stringWithFormat:(@"%@ %@", eightOfDiamonds.rank, eightOfDiamonds.suit)];
+    self.topLabel.text = stringIWantToDisplay;
+    self.middleLabel.text = stringIWantToDisplay;
+    self.bottomLabel.text = stringIWantToDisplay;
+
 }
 
 - (IBAction)tenOfHearts:(id)sender {
     PlayingCard *tenOfHearts = [[PlayingCard alloc] initWithSuit:@"♥️" rank:@10];
+    
+    NSString *stringIWantToDisplay = [NSString stringWithFormat:(@"%@ %@", tenOfHearts.rank, tenOfHearts.suit)];
+    self.topLabel.text = stringIWantToDisplay;
+    self.middleLabel.text = stringIWantToDisplay;
+    self.bottomLabel.text = stringIWantToDisplay;
+
 }
 
 @end
